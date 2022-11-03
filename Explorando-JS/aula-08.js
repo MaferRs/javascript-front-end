0427fb4d45317dc9347123d53a7cb1b339392870
Maria Fernanda <90789503+MaferRs@users.noreply.github.com>
2022-11-02 21:06:55 -0300

    aula 08

---
+++ Explorando-JS/aula-08.js
@@ -0,0 +1,29 @@
+console.log(`Trabalhando com condicionais`)
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+
+)
+
+const idadeComprador = 18// 15
+const estaAcompanhada = false
+let temPassagemComprada = false
+const destino = "Rio de Janeiro"
+
+console.log(`Destinos possíveis: ${listaDeDestinos} `)
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada
+
+let contador = 0
+let destinoExiste = false
+while (contador < 3) {
+
+    if (listaDeDestinos[contador] == destino) {
+        console.log("O Destino Existe")
+        destinoExiste = true
+        break
+    }
+    contador += 1
+}
+console.log("Destino existe: ", destinoExiste)
