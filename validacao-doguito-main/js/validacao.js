e829a9c8f55383f7204b63b4f4b0b3937cfac1e0
Maria Fernanda <90789503+MaferRs@users.noreply.github.com>
2022-11-13 18:29:04 -0300

    validação do cpf

--- validacao-doguito-main/js/validacao.js
+++ validacao-doguito-main/js/validacao.js
@@ -5,10 +5,21 @@ export function valida(input) {
     }
     if (input.validity.valid) {
         input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelectpr('.input-menasgem-erro').innerHTML = ""
     } else {
         input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelectpr('.input-menasgem-erro').innerHTML = mostraMensagemDeErro
+            (tipoDeInput, input)
     }
 }
+
+const tiposDeErro = [
+    'valueMissing',
+    ' typeMismatch',
+    'patternMismatch',
+    'customError'
+
+]
 
 
 const mensagensDeErro = {
@@ -26,21 +37,36 @@ const mensagensDeErro = {
     dataNascimmento: {
         valueMissing: 'O campo de data de nascimento não pode estar vazio',
         customError: 'Você deve ser maior de 18 anos para se cadastrar'
+    },
+    cpf: {
+        valueMissing: 'O campo de cpf não pode estar vazio',
+        customError: 'O CPF digitado não é válido'
     }
+
 }
 
 const validadores = {
-    dataNascimento: input => validaDataNascimento(input)
+    dataNascimento: input => validaDataNascimento(input),
+    cpf: input => validaCPF(input)
 
 }
 
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = " "
+    tiposDeErro.forEach(erro => {
+        if (input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    return mensagem
+}
 
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value)
     let mensagem = ""
     if (!maiorQue18(dataRecebida)) {
-        mensagem = "Você deve ser maior que 1 anos para se cadastrar."
+        mensagem = "Você deve ser maior que 1 8anos para se cadastrar."
     }
     input.setCustomValidity(mensagem)
 }
@@ -52,5 +78,68 @@ function maiorQue18(data) {
     const dataMais18 = new Date(data.getUTCFullYear() + 18, datagetUTCMonth(), datagetUTCDate())
     return dataMais18 < dataAtual
 
-
 }
+function validaCPF(input) {
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ""
+
+    if (!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
+        mensagem = " O CPF digitado não é válido"
+    }
+    input.setCustomValidity(mensagem)
+}
+
+function checaCPFRepetido(cpf) {
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999',
+    ]
+    let cpfValido = true
+
+    valoresRepetidos.forEach(valor => {
+        if (valor == cpf) {
+            cpfValido = false
+        }
+    })
+    return cpfValido
+}
+function checaEstruturaCPF(cpf) {
+    const multiplicador = 10
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+function checaCPFRepetido(cpf, multiplicador) {
+    if (multiplicador >= 12) {
+        return true
+    }
+
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split(" ")
+    const checaDigitoVerificador = cpf.charAt(multiplicador - 1)
+    for (let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--) {
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+    if (checaDigitoVerificador == confirmaDigito(soma)) {
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+    return false
+}
+
+
+function confirmaDigito(soma) {
+    return 11 - (soma % 11)
+}
+
+// 123 456 789 09
+// let soma = (11 * 1) + (10 * 2) + (9 * 3) ... (2 * 0)
+// let difitoVerificar = 11 - (soma % 11)
